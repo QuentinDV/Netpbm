115d279750cd5d4d306d10f7414acf930be81426
daquentin <quentin.dassivignon@ynov.com>
2024-01-11 12:05:16 +0100

    git add .

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/QuentinDV/Netpbm/functions"
+
+func main() {
+
+	// functions.PBMfunction("Essai2")
+	// functions.PGMfunction("apollonian_gasket.ascii")
+	// functions.PGMfunction("imagetest.1.128")
+	functions.PGMfunction("lena.ascii")
+
+}
